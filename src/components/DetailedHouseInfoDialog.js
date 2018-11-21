a4e33ca6da920f41b26fd8092978e1549e324d67
atsyganenko <M0ntparnasse>
2018-11-21 03:35:05 +0100

    house details dialog extended with data

--- src/components/DetailedHouseInfoDialog.js
+++ src/components/DetailedHouseInfoDialog.js
@@ -4,12 +4,33 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import URLtoNameResolver from '../containers/URLtoNameResolver';
+import Table from '@material-ui/core/Table';
+import TableCell from '@material-ui/core/TableCell';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import {withStyles} from '@material-ui/core/styles';
+import URLtoNameResolver from "../containers/URLtoNameResolver";
+
+const styles = (theme) => {
+    return {
+        key: {
+            ...theme.typography.subtitle2,
+        }
+    };
+};
 
 const DetailedHouseInfoDialog = (props) => {
 
-
     const {open, onClose, houseDetails} = props;
+    const textFields = ['name', 'region', 'coatOfArms', 'words', 'titles',
+        'seats', 'founded', 'diedOut', 'ancestralWeapons'];
+    const urls = ['currentLord', 'founder', 'heir', 'overlord'];
+
+    const _hasValue = (key) => {
+        const isEmptyArray = Array.isArray(houseDetails[key])
+            && houseDetails[key].filter(val => val !== "").length === 0;
+        return houseDetails[key] && houseDetails[key] !== "" && !isEmptyArray;
+    };
 
     return (
         <Dialog
@@ -20,7 +41,27 @@ const DetailedHouseInfoDialog = (props) => {
             <DialogTitle id="house-dialog">{houseDetails.name ? houseDetails.name : "No data"}</DialogTitle>
             <DialogContent>
                 <DialogContentText id="alert-dialog-description">
-                    <URLtoNameResolver url={houseDetails.currentLord}/>
+                    <Table>
+                        <TableBody>
+                            {textFields.map((key, index) => {
+                                return (
+                                    <TableRow key={index}>
+                                        <TableCell className={props.classes.key}>{key}</TableCell>
+                                        <TableCell>{_hasValue(key) ? props.houseDetails[key]
+                                            : 'Unknown'}</TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                            {urls.map((key, index) => {
+                                return (
+                                    <TableRow key={index}>
+                                        <TableCell className={props.classes.key}>{key}</TableCell>
+                                        <TableCell><URLtoNameResolver url={props.houseDetails[key]}/></TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
                 </DialogContentText>
             </DialogContent>
         </Dialog>
@@ -33,4 +74,4 @@ DetailedHouseInfoDialog.propTypes = {
     houseDetails: PropTypes.object.isRequired
 };
 
-export default DetailedHouseInfoDialog;
+export default withStyles(styles)(DetailedHouseInfoDialog);
